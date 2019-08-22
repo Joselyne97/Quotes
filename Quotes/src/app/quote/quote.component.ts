5b27955611c4e595d6b89cf136b33ebe332b2e83
Joselyne97 <joselynejojo740@gmail.com>
2019-08-22 18:36:23 +0200

    I added new quotes and delete the name and description words

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -6,13 +6,13 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  Quotes:Quote[] = [
-    {id:1,name:'There is nothing permanent except change', description: 'It was quoted by Heraclitus'},
-    {id:2,name:'Let us sacrifice ourselfs today so that our children can have a better tomorrow', description: 'It was quoted by A.P.J.Abdul Kalam'},
-    {id:3,name:'There is no charm equal to tenderness of heart', description: 'It was quoted by Jane Austen'},
-    {id:4,name:'All that we see or seem is but a dream within a dream', description: 'It was quoted by Edgar Allan Poe'},
-    {id:5,name:'The only journey is the one within', description: 'It was quoted by Rainer Maria Rilke'},
-    {id:6,name:'Think in the morning, act in the noon, eat in the evening and sleep in the night', description: 'It was quoted by Will Rogers'},
+  quotes:Quote[] = [
+    new Quote (1, 'There is nothing permanent except change', 'It was quoted by Heraclitus'),
+    new Quote (2, 'Let us sacrifice ourselfs today so that our children can have a better tomorrow', 'It was quoted by A.P.J.Abdul Kalam'),
+    new Quote (3, 'There is no charm equal to tenderness of heart', 'It was quoted by Jane Austen'),
+    new Quote (4, 'All that we see or seem is but a dream within a dream', 'It was quoted by Edgar Allan Poe'),
+    new Quote (5, 'The only journey is the one within', 'It was quoted by Rainer Maria Rilke'),
+    new Quote (6, 'Think in the morning, act in the noon, eat in the evening and sleep in the night', 'It was quoted by Will Rogers'),
   ];
   constructor() { }
 
